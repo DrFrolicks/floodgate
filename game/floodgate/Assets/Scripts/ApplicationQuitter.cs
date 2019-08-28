7f793a848d42b440e37e7aad9d99048e67c22752
Evan Cheng <dr.frolicks@gmail.com>
2019-08-28 14:52:24 -0700

    near gold no phrase tracking bug

---
+++ game/floodgate/Assets/Scripts/ApplicationQuitter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Quits the application on start.
+/// </summary>
+public class ApplicationQuitter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.Quit();
+    }
+
+}
