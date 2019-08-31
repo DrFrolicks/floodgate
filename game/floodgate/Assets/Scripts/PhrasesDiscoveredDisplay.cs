dfb06ad51fff92be04e395b12544d88e28077363
Evan Cheng <dr.frolicks@gmail.com>
2019-08-30 23:53:32 -0700

    gold

--- game/floodgate/Assets/Scripts/PhrasesDiscoveredDisplay.cs
+++ game/floodgate/Assets/Scripts/PhrasesDiscoveredDisplay.cs
@@ -9,16 +9,31 @@ public class PhrasesDiscoveredDisplay : MonoBehaviour
     [SerializeField]
     string templateString; 
 
-
-    private void Start()
+    
+    private void Awake()
     {
         templateString = GetComponent<TextMeshProUGUI>().text; 
-        GameManager.Instance.OnPhraseDiscovered.AddListener(UpdateText); 
     }
-    
-    public void UpdateText()
+
+    private void OnEnable()
+    {
+        UpdateTextOnce();
+        
+    }
+
+    public void UpdateTextWithTemplate()
     {
         TextMeshProUGUI TMPtext = GetComponent<TextMeshProUGUI>();
-        TMPtext.text = templateString.Replace("%discovered", StringUtil.NumberToWords(GameManager.Instance.phrasesDiscovered)); 
+        TMPtext.text = templateString.Replace("%discovered", StringUtil.NumberToWords(GameManager.Instance.phrasesDiscovered));
     }
+
+    /// <summary>
+    /// Updates the text with the current text as the template text. Can only be called once while the template is displayed.
+    /// </summary>
+    void UpdateTextOnce()
+    {
+        templateString = GetComponent<TextMeshProUGUI>().text;
+        UpdateTextWithTemplate(); 
+    }
+       
 }
